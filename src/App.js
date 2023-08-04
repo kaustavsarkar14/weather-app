70a9991897ab22af0da82b2a8005b82cbf7438a4
kaustavsarkar14 <kaustav1.sarkar@aot.edu.in>
2023-08-04 22:52:21 +0530

    loading screen added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './components/Card';
 import Input from './components/Input';
@@ -7,23 +7,27 @@ import { getWeatherData } from './api';
 
 function App() {
   const weather = useWeather()
-  // console.log(weather)
+  const[loading, setLoading] = useState(true);
 
   useEffect(()=>{
     const res = getWeatherData('new delhi').then(data=>weather.setData(data))
   },[])
   useEffect(()=>{
     weather.getUserLocation()
+    setTimeout(() => {
+      setLoading(false)
+    }, 4000);
   },[])
 
 
+    if(loading) return (<img style={{margin:"auto", display:"block", marginTop:"5rem", width:"100vw"}} src='https://i.pinimg.com/originals/53/32/38/533238dd9a8ebfeaf7a8f4d4c2cabab6.gif' />)
 
   return (
     <div className="App">
       <Input/>
       <button className='btn' disabled={!weather.search} value="Search" onClick={weather.fetchData}>Search</button>
      <Card/>
-     <button className='btn'>Refresh</button>
+     
 
     </div>
   );
