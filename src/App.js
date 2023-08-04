b53338267c4f22dd57fabe56897969462a0af277
kaustavsarkar14 <kaustav1.sarkar@aot.edu.in>
2023-08-04 23:06:50 +0530

    loader size fix

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   },[])
 
 
-    if(loading) return (<img style={{margin:"auto", display:"block", marginTop:"5rem", width:"100vw"}} src='https://i.pinimg.com/originals/53/32/38/533238dd9a8ebfeaf7a8f4d4c2cabab6.gif' />)
+    if(loading) return (<img style={{margin:"auto", display:"block", marginTop:"5rem", width:"100vw", maxWidth:"35rem"}} src='https://i.pinimg.com/originals/53/32/38/533238dd9a8ebfeaf7a8f4d4c2cabab6.gif' />)
 
   return (
     <div className="App">
